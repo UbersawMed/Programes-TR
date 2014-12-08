a4fe2e2b935d64a28adf89b1c6c64b0d19edb88f
UbersawMed <alexcp99@gmail.com>
2014-12-08 18:29:26 +0100

    Base del primer programa

---
+++ ProgramesEnsI/Cesar.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//#include <string>
+//#include <math.h>
+//#include <vector>
+//#include <list>
+//#include <iomanip>
+//#include <sstream>
+using namespace std;
+
+
+int main() {
+	
+	int i = 4;
+
+	cout << i << endl;
+
+}
+
